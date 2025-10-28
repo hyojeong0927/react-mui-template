8d69a4caf86595c3f09ae107f36aa85233c30569
Hyojeong Kim <hyokim0927@gmail.com>
2025-10-28 21:48:36 +0900

    빌드 경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import App from './App';
+import PubMain from '@/publish/PubMain.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PubMain />
   </StrictMode>,
 );
